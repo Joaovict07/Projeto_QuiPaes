9a48adf9f53c488b17d5656b4d531354f6babcf5
Zifts <galaxyalien777@gmail.com>
2025-09-29 22:41:39 -0300

    Criando entidade de compras

---
+++ src/main/java/org/api/padariaapi/entity/Compra.java
@@ -0,0 +1,26 @@
+package org.api.padariaapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Setter;
+import lombok.Getter;
+
+
+@Entity
+@Table(name = "quiPaesCompra")
+
+@Getter
+@Setter
+public class Compra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCompra;
+    private String cdProduto;
+    private Long DataHora;
+    private String Status; //Status tipo "aprovado" ou "cancelado"
+    private String StatusEntrega;
+
+
+
+
+
+}
