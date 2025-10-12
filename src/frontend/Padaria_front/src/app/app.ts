6f1b73c93dc13f00a2882e29a8b97bed8f1d6cd9
Christian Ribeiro Gama Franco <chrisribgama3@gmail.com>
2025-10-12 19:18:13 -0300

    Adicionando Componente Sobre e modificando Carrossel.

--- src/frontend/Padaria_front/src/app/app.ts
+++ src/frontend/Padaria_front/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { BarraMenu } from './components/barra-menu/barra-menu';
 import {Rodape} from './components/rodape/rodape';
 import { Produtos } from './components/produtos/produtos'
+import { Sobre } from './components/sobre/sobre'
 
 @Component({
   selector: 'app-root',
-  imports: [BarraMenu, Rodape, Produtos],
+  imports: [BarraMenu, Rodape, Produtos, Sobre],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
