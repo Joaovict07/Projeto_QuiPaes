17d360c78b3871a4ceea30fa933913e6f9a3d015
Beatriz Sanches <bia.olive1234@gmail.com>
2025-11-16 19:21:41 -0300

    pergunta de segurança

--- src/frontend/Padaria_front/.angular/cache/20.3.3/Padaria_front/vite/deps_ssr/@angular_router.js
+++ src/frontend/Padaria_front/.angular/cache/20.3.3/Padaria_front/vite/deps_ssr/@angular_router.js
@@ -77,10 +77,10 @@ import {
   withRouterConfig,
   withViewTransitions,
   ɵEmptyOutletComponent
-} from "./chunk-OXN6S6T7.js";
-import "./chunk-EBP5UPV7.js";
-import "./chunk-MFG5HXI7.js";
+} from "./chunk-U73GZLCO.js";
+import "./chunk-UAVTKWVU.js";
 import "./chunk-ZB5G2ADV.js";
+import "./chunk-MFG5HXI7.js";
 import "./chunk-B4LPUZWY.js";
 import "./chunk-LCC53DGS.js";
 import "./chunk-ME6LV6BC.js";
